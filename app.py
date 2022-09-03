1e10d4c61d5e2ed0923a48648dd337866c6e5dd3
brdy1 <brdy724@gmail.com>
2022-09-03 10:25:59 -0700

    bug fix

--- app.py
+++ app.py
@@ -631,7 +631,7 @@ def randoEvolution(parameters):
         monname = parameters
         limit = 10
     elif len(parameters.split(" ")) == 2:
-        parameters.lower().replace("mime jr","MimeJr")
+        parameters = parameters.title().replace("Mime Jr","MimeJr")
         parameters = parameters.split(" ")
         try:
             monname = str(parameters[0])
