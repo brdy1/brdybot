6699dd654f5cc1cc6c4d6bac5ba53a294ea1455b
brdy1 <brdy724@gmail.com>
2023-05-13 02:00:42 -0700

    gitignore VS Studio & BotService C#

---
+++ BotService/APIService/Service1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace APIService
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        private Process _process;
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            // Start the first Python script
+            var startInfo1 = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files\Python37\python.exe",
+                Arguments = @"C:\Users\Administrator\brdybot\app.py",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            _process = Process.Start(startInfo1);
+
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            // Stop the Python scripts
+            _process?.Kill();
+        }
+
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+
+            // Stop the Python scripts
+            _process?.Kill();
+        }
+
+        private void InitializeComponent()
+        {
+            // 
+            // Service1
+            // 
+            this.ServiceName = "APIService";
+
+        }
+    }
+}
